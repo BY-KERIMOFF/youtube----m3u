460a49e0eadbc9076e9b9ac2717b7298be22f8b7
LOST-LOVERS <tebrizkerimli8794@gmail.com>
2025-06-07 21:31:16 +0400

    Update youtube_latest.py
    Signed-off-by: LOST-LOVERS <tebrizkerimli8794@gmail.com>

--- youtube_latest.py
+++ youtube_latest.py
@@ -8,7 +8,6 @@ def get_channel_id(custom_url):
     if response.status_code != 200:
         print("Kanal səhifəsi tapılmadı.")
         return None
-    
     match = re.search(r'"channelId":"(UC[\w-]{22})"', response.text)
     if match:
         return match.group(1)
@@ -22,7 +21,6 @@ def get_latest_video_link(channel_id):
     if response.status_code != 200:
         print("RSS feed tapılmadı.")
         return None
-    
     root = ET.fromstring(response.content)
     video = root.find('{http://www.w3.org/2005/Atom}entry')
     if video is not None:
@@ -33,6 +31,12 @@ def get_latest_video_link(channel_id):
         print("Video tapılmadı.")
         return None
 
+def save_m3u(title, link):
+    with open("latest.m3u", "w", encoding="utf-8") as f:
+        f.write("#EXTM3U\n")
+        f.write(f"#EXTINF:-1,{title}\n")
+        f.write(f"{link}\n")
+
 if __name__ == "__main__":
     custom_url = "@cennetmahallesishowtv"
     channel_id = get_channel_id(custom_url)
@@ -41,3 +45,4 @@ if __name__ == "__main__":
         if result:
             title, link = result
             print(f"Son video:\nBaşlıq: {title}\nLink: {link}")
+            save_m3u(title, link)
