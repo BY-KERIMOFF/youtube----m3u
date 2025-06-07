8e57d39148a71fbcba79ac2db59cce8785ee0e95
LOST-LOVERS <tebrizkerimli8794@gmail.com>
2025-06-07 20:54:53 +0400

    Update generate_playlist.py
    Signed-off-by: LOST-LOVERS <tebrizkerimli8794@gmail.com>

--- generate_playlist.py
+++ generate_playlist.py
@@ -3,7 +3,7 @@ import yt_dlp
 channels = [
     ("SHOW MAX", "https://www.youtube.com/@showtv/live"),
     ("YASAK ELMA", "https://www.youtube.com/@YasakElma/live"),
-    # Buraya istədiyin qədər əlavə et...
+    # ... digər kanallar ...
 ]
 
 def fetch_stream_url(youtube_url):
